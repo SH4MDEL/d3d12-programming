b75a634d1607bcc472fc320055af10559a8d37b0
Seungjun Lee <45813451+SH4MDEL@users.noreply.github.com>
2022-04-03 23:32:18 +0900

    LabProject 02
    작성중인 프로젝트 02 커밋

---
+++ LabProjects/LabProject02/GameFramework.cpp
@@ -0,0 +1,93 @@
+#include "stdafx.h"
+#include "GameFramework.h"
+
+CGameFramework::CGameFramework()
+{
+	m_pdxgiFactory = NULL;
+	m_pdxgiSwapChain = NULL;
+	m_pd3dDevice = NULL;
+
+	m_pd3dCommandAllocator = NULL;
+	m_pd3dCommandQueue = NULL;
+	m_pd3dPipelineState = NULL;
+	m_pd3dCommandList = NULL;
+
+	for (int i = 0; i < m_nSwapChainBuffers; i++) m_ppd3dRenderTargetBuffers[i] = NULL;
+	m_pd3dRtvDescriptorHeap = NULL;
+	m_nRtvDescriptorIncrementSize = 0;
+
+	m_pd3dDepthStencilBuffer = NULL;
+	m_pd3dDsvDescriptorHeap = NULL;
+	m_nDsvDescriptorIncrementSize = 0;
+
+	m_nSwapChainBufferIndex = 0;
+
+	m_hFenceEvent = NULL;
+	m_pd3dFence = NULL;
+	m_nFenceValue = 0;
+
+	m_nWndClientWidth = FRAME_BUFFER_WIDTH;
+	m_nWndClientHeight = FRAME_BUFFER_HEIGHT;
+}
+
+// 다음 함수는 응용 프로그램이 실행되어 주 윈도우가 생성되면 호출된다는 것에 유의하라.
+bool CGameFramework::OnCreate(HINSTANCE hInstance, HWND hMainWnd)
+{
+	m_hInstance = hInstance;
+	m_hWnd = hMainWnd;
+
+// Direct3D 디바이스, 명령 큐와 명령 리스트, 스왑 체인 등을 생성하는 함수를 호출한다.
+	CreateDirect3DDevice();
+	CreateCommandQueueAndList();
+	CreateSwapChain();
+	CreateRtvAndDsvDescriptorHeaps();
+	CreateRenderTargetViews();
+	CreateDepthStencilView();
+
+	// 렌더링할 게임 객체를 생성한다.
+	BuildObjects();
+
+	return true;
+}
+
+void CGameFramework::OnDestroy()
+{
+	// GPU가 모든 명령 리스트를 실행할 때 까지 기다린다.
+	WaitForGpuComplete();
+
+	// 게임 객체(게임 월드 객체)를 소멸한다.
+	ReleaseObjects();
+
+	::CloseHandle(m_hFenceEvent);
+
+	for (int i = 0; i < m_nSwapChainBuffers; i++) if (m_ppd3dRenderTargetBuffers[i])
+		m_ppd3dRenderTargetBuffers[i]->Release();
+	if (m_pd3dRtvDescriptorHeap) m_pd3dRtvDescriptorHeap->Release();
+	
+	if (m_pd3dDepthStencilBuffer) m_pd3dDepthStencilBuffer->Release();
+	if (m_pd3dDsvDescriptorHeap) m_pd3dDsvDescriptorHeap->Release();
+
+	if (m_pd3dCommandAllocator) m_pd3dCommandAllocator->Release();
+	if (m_pd3dCommandQueue) m_pd3dCommandQueue->Release();
+	if (m_pd3dPipelineState) m_pd3dPipelineState->Release();
+	if (m_pd3dCommandList) m_pd3dCommandList->Release();
+
+	if (m_pd3dFence) m_pd3dFence->Release();
+
+	m_pdxgiSwapChain->SetFullscreenState(FALSE, NULL);
+	if (m_pdxgiSwapChain) m_pdxgiSwapChain->Release();
+	if (m_pd3dDevice) m_pd3dDevice->Release();
+	if (m_pdxgiFactory) m_pdxgiFactory->Release();
+
+#if defined(_DEBUG)
+	IDXGIDebug1* pdxgiDebug = NULL;
+	DXGIGetDebugInterface1(0, __uuidof(IDXGIDebug1), (void**)&pdxgiDebug);
+	HRESULT hResult = pdxgiDebug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_DETAIL);
+	pdxgiDebug->Release();
+#endif
+}
+
+CGameFramework::~CGameFramework()
+{
+
+}
