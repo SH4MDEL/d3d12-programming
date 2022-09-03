cecff2c5e540e0ca438a7b86b4031872b2aa282f
Seungjun Lee <45813451+SH4MDEL@users.noreply.github.com>
2022-09-04 00:30:50 +0900

    end project05: 회전하는 삼각형 그리기
    Update 함수를 호출하여 회전하는 삼각형 구현.
    player와 camera를 추가하여 서로 연결. scene은 player를 가지고 있고, player와 camera는 각각 서로를 가지고 있음.
    camera의 eye는 항상 player를 향함.

---
+++ d3d12-framework-project/project05/shader.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "stdafx.h"
+
+class Shader
+{
+public:
+	Shader();
+	Shader(const ComPtr<ID3D12Device>& device, const ComPtr<ID3D12RootSignature>& rootSignature);
+	~Shader() = default;
+
+	ComPtr<ID3D12PipelineState> GetPipelineState() const { return m_pipelineState; }
+
+protected:
+	ComPtr<ID3D12PipelineState>			m_pipelineState;
+	vector<D3D12_INPUT_ELEMENT_DESC>	m_inputLayout;
+};
+
