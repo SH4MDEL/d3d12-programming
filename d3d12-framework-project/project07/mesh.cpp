4defed0a41edbe1023fcc5bc52faf7faa16ce3b7
Seungjun Lee <45813451+SH4MDEL@users.noreply.github.com>
2022-09-04 16:55:06 +0900

    project07: 인스턴싱을 통해 회전하는 정육면체 그리기
    mesh나 shader의 관리를 object에서 하지 말고 별도의 클래스를 만들자.
    object는 정점 정보나 회전 정보만을 관리한다.

---
+++ d3d12-framework-project/project07/mesh.cpp
@@ -0,0 +1,93 @@
+#include "mesh.h"
+
+Mesh::Mesh(const ComPtr<ID3D12Device>& device, const ComPtr<ID3D12GraphicsCommandList>& commandList, const vector<Vertex>& vertices, const vector<UINT>& indices)
+{
+	// 정점 버퍼 생성
+	m_nVertices = vertices.size();
+	const UINT vertexBufferSize = sizeof(Vertex) * vertices.size();
+
+	DX::ThrowIfFailed(device->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+		D3D12_HEAP_FLAG_NONE,
+		&CD3DX12_RESOURCE_DESC::Buffer(vertexBufferSize),
+		D3D12_RESOURCE_STATE_COPY_DEST,
+		NULL,
+		IID_PPV_ARGS(&m_vertexBuffer)));
+
+	DX::ThrowIfFailed(device->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
+		D3D12_HEAP_FLAG_NONE,
+		&CD3DX12_RESOURCE_DESC::Buffer(vertexBufferSize),
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		NULL,
+		IID_PPV_ARGS(&m_vertexUploadBuffer)));
+
+	// DEFAULT 버퍼에 데이터 복사
+	D3D12_SUBRESOURCE_DATA vertextData{};
+	vertextData.pData = vertices.data();
+	vertextData.RowPitch = vertexBufferSize;
+	vertextData.SlicePitch = vertextData.RowPitch;
+	UpdateSubresources<1>(commandList.Get(), m_vertexBuffer.Get(), m_vertexUploadBuffer.Get(), 0, 0, 1, &vertextData);
+
+	// 정점 버퍼 상태 변경
+	commandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(m_vertexBuffer.Get(), D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER));
+
+	// 정점 버퍼 뷰 설정
+	m_vertexBufferView.BufferLocation = m_vertexBuffer->GetGPUVirtualAddress();
+	m_vertexBufferView.SizeInBytes = vertexBufferSize;
+	m_vertexBufferView.StrideInBytes = sizeof(Vertex);
+
+	//--------------------------------------------------------------------
+
+	m_nIndices = indices.size();
+	if (m_nIndices) {
+		const UINT indexBufferSize = sizeof(UINT) * indices.size();
+
+		DX::ThrowIfFailed(device->CreateCommittedResource(
+			&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+			D3D12_HEAP_FLAG_NONE,
+			&CD3DX12_RESOURCE_DESC::Buffer(indexBufferSize),
+			D3D12_RESOURCE_STATE_COPY_DEST,
+			NULL,
+			IID_PPV_ARGS(&m_indexBuffer)));
+
+		DX::ThrowIfFailed(device->CreateCommittedResource(
+			&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
+			D3D12_HEAP_FLAG_NONE,
+			&CD3DX12_RESOURCE_DESC::Buffer(indexBufferSize),
+			D3D12_RESOURCE_STATE_GENERIC_READ,
+			NULL,
+			IID_PPV_ARGS(&m_indexUploadBuffer)));
+
+		D3D12_SUBRESOURCE_DATA indexData{};
+		indexData.pData = indices.data();
+		indexData.RowPitch = indexBufferSize;
+		indexData.SlicePitch = indexData.RowPitch;
+		UpdateSubresources<1>(commandList.Get(), m_indexBuffer.Get(), m_indexUploadBuffer.Get(), 0, 0, 1, &indexData);
+
+		commandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(m_indexBuffer.Get(), D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_INDEX_BUFFER));
+
+		m_indexBufferView.BufferLocation = m_indexBuffer->GetGPUVirtualAddress();
+		m_indexBufferView.Format = DXGI_FORMAT_R32_UINT;
+		m_indexBufferView.SizeInBytes = indexBufferSize;
+	}
+}
+
+void Mesh::Render(const ComPtr<ID3D12GraphicsCommandList>& m_commandList) const
+{
+	m_commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	m_commandList->IASetVertexBuffers(0, 1, &m_vertexBufferView);
+	if (m_nIndices) {
+		m_commandList->IASetIndexBuffer(&m_indexBufferView);
+		m_commandList->DrawIndexedInstanced(m_nIndices, 1, 0, 0, 0);
+	}
+	else {
+		m_commandList->DrawInstanced(m_nVertices, 1, 0, 0);
+	}
+}
+
+void Mesh::ReleaseUploadBuffer()
+{
+	if (m_vertexUploadBuffer) m_vertexUploadBuffer.Reset();
+	if (m_indexUploadBuffer) m_indexUploadBuffer.Reset();
+}
---
+++ d3d12-framework-project/project07/mesh.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "stdafx.h"
+
+class Mesh
+{
+public:
+	Mesh(const ComPtr<ID3D12Device>& device, const ComPtr<ID3D12GraphicsCommandList>& commandList, const vector<Vertex>& vertices, const vector<UINT>& indices);
+	~Mesh() = default;
+
+	void Render(const ComPtr<ID3D12GraphicsCommandList>& m_commandList) const;
+	void ReleaseUploadBuffer();
+
+private:
+	UINT						m_nVertices;
+	ComPtr<ID3D12Resource>		m_vertexBuffer;
+	ComPtr<ID3D12Resource>		m_vertexUploadBuffer;
+	D3D12_VERTEX_BUFFER_VIEW	m_vertexBufferView;
+
+	UINT						m_nIndices;
+	ComPtr<ID3D12Resource>		m_indexBuffer;
+	ComPtr<ID3D12Resource>		m_indexUploadBuffer;
+	D3D12_INDEX_BUFFER_VIEW		m_indexBufferView;
+};
