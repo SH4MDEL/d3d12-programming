a861c9279de0adb2fda68c7309bec59b9fe5399f
Seungjun Lee <beeutiful@naver.com>
2022-11-16 15:05:07 +0900

    project15-1
    적과의 전투 구현

---
+++ d3d12-framework-project/project15-1/framework.cpp
@@ -0,0 +1,420 @@
+#include "framework.h"
+
+GameFramework::GameFramework(UINT width, UINT height) : 
+	m_width(width), 
+	m_height(height), 
+	m_frameIndex{0},
+	m_viewport{0.0f, 0.0f, (FLOAT)width, (FLOAT)height, 0.0f, 1.0f},
+	m_scissorRect{0, 0, (LONG)width, (LONG)height}, 
+	m_rtvDescriptorSize {0}
+{
+	m_aspectRatio = (FLOAT)width / (FLOAT)height;
+}
+
+GameFramework::~GameFramework()
+{
+
+}
+
+void GameFramework::OnCreate(HINSTANCE hInstance, HWND hWnd)
+{
+	m_hInstance = hInstance;
+	m_hWnd = hWnd;
+
+	StartPipeline();
+	BuildObjects(); 
+}
+
+void GameFramework::OnDestroy()
+{
+	WaitForGpuComplete();
+
+	::CloseHandle(m_fenceEvent);
+}
+
+void GameFramework::OnProcessingMouseMessage() const
+{
+	if (m_scene) m_scene->OnProcessingMouseMessage(m_hWnd, m_width, m_height, m_timer.GetDeltaTime());
+}
+
+void GameFramework::OnProcessingKeyboardMessage() const
+{
+	if (m_scene) m_scene->OnProcessingKeyboardMessage(m_timer.GetDeltaTime());
+}
+
+void GameFramework::StartPipeline()
+{
+	UINT dxgiFactoryFlags = 0;
+#if defined(_DEBUG)
+	ComPtr<ID3D12Debug> DebugController;
+	if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&DebugController))))
+	{
+		DebugController->EnableDebugLayer();
+		dxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
+	}
+#endif
+	ComPtr<IDXGIFactory4> m_factory;
+	DX::ThrowIfFailed(CreateDXGIFactory2(dxgiFactoryFlags, IID_PPV_ARGS(&m_factory)));
+
+	// 1. 디바이스 생성
+	CreateDevice();
+
+	// 2. 펜스 객체 생성
+	CreateFence();
+
+	// 3. 4X MSAA 품질 수준 지원 여부 점검
+	Check4xMSAAMultiSampleQuality();
+
+	// 4. 명령 큐, 명령 할당자, 명령 리스트 생성
+	CreateCommandQueueAndList();
+
+	// 5. 스왑 체인 생성
+	CreateSwapChain();
+
+	// 6. 서술자 힙 생성
+	CreateRtvDsvDescriptorHeap();
+
+	// 7. 후면 버퍼에 대한 렌더 타겟 뷰 생성
+	CreateRenderTargetView();
+
+	// 8. 깊이 스텐실 버퍼, 깊이 스텐실 뷰 생성
+	CreateDepthStencilView();
+
+	// 9. 루트 시그니처 생성
+	CreateRootSignature();
+}
+
+void GameFramework::CreateDevice()
+{
+	DX::ThrowIfFailed(CreateDXGIFactory1(IID_PPV_ARGS(&m_factory)));
+
+	// 하드웨어 어댑터를 나타내는 장치를 생성해 본다.
+	// 최소 기능 수준은 D3D_FEATURE_LEVEL_12_0 이다.
+	ComPtr<IDXGIAdapter1> Adapter;
+	for (UINT i = 0; DXGI_ERROR_NOT_FOUND != m_factory->EnumAdapters1(i, &Adapter); ++i)
+	{
+		DXGI_ADAPTER_DESC1 adapterDesc;
+		Adapter->GetDesc1(&adapterDesc);
+		if (adapterDesc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE) continue;
+		if (SUCCEEDED(D3D12CreateDevice(Adapter.Get(), D3D_FEATURE_LEVEL_12_0, IID_PPV_ARGS(&m_device)))) break;
+	}
+
+	// 실패했다면 WARP 어댑터를 나타내는 장치를 생성한다.
+	if (!m_device)
+	{
+		m_factory->EnumWarpAdapter(IID_PPV_ARGS(&Adapter));
+		DX::ThrowIfFailed(D3D12CreateDevice(Adapter.Get(), D3D_FEATURE_LEVEL_12_0, IID_PPV_ARGS(&m_device)));
+	}
+}
+
+void GameFramework::CreateFence()
+{
+	// CPU와 GPU의 동기화를 위한 Fence 객체를 생성한다.
+	DX::ThrowIfFailed(m_device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_fence)));
+	m_fenceEvent = CreateEvent(nullptr, false, false, nullptr);
+	m_fenceValue = 1;
+}
+
+void GameFramework::Check4xMSAAMultiSampleQuality()
+{
+	D3D12_FEATURE_DATA_MULTISAMPLE_QUALITY_LEVELS msQualityLevels;
+	msQualityLevels.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	msQualityLevels.SampleCount = 4;
+	msQualityLevels.Flags = D3D12_MULTISAMPLE_QUALITY_LEVELS_FLAG_NONE;
+	msQualityLevels.NumQualityLevels = 0;
+	DX::ThrowIfFailed(m_device->CheckFeatureSupport(D3D12_FEATURE_MULTISAMPLE_QUALITY_LEVELS, &msQualityLevels, sizeof(msQualityLevels)));
+	m_MSAA4xQualityLevel = msQualityLevels.NumQualityLevels;
+
+	assert(m_MSAA4xQualityLevel > 0 && "Unexpected MSAA Quality Level..");
+}
+
+void GameFramework::CreateCommandQueueAndList()
+{
+	D3D12_COMMAND_QUEUE_DESC queueDesc{};
+	queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+	queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+	// 명령 큐 생성
+	DX::ThrowIfFailed(m_device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&m_commandQueue)));
+	// 명령 할당자 생성
+	DX::ThrowIfFailed(m_device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_commandAllocator)));
+	// 명령 리스트 생성
+	DX::ThrowIfFailed(m_device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, m_commandAllocator.Get(), nullptr, IID_PPV_ARGS(&m_commandList)));
+	// Reset을 호출하기 때문에 Close 상태로 시작
+	DX::ThrowIfFailed(m_commandList->Close());
+}
+
+void GameFramework::CreateSwapChain()
+{
+	DXGI_SWAP_CHAIN_DESC sd{};
+	sd.BufferDesc.Width = m_width;
+	sd.BufferDesc.Height = m_height;
+	sd.BufferDesc.RefreshRate.Numerator = 60;
+	sd.BufferDesc.RefreshRate.Denominator = 1;
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	sd.SampleDesc.Count = m_MSAA4xQualityLevel > 1 ? 4 : 1;
+	sd.SampleDesc.Quality = m_MSAA4xQualityLevel > 1 ? m_MSAA4xQualityLevel - 1 : 0;
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	sd.BufferCount = SwapChainBufferCount;
+	sd.OutputWindow = m_hWnd;
+	sd.Windowed = true;
+	sd.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+	sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+
+	ComPtr<IDXGISwapChain> swapChain;
+	DX::ThrowIfFailed(m_factory->CreateSwapChain(m_commandQueue.Get(), &sd, &swapChain));
+	DX::ThrowIfFailed(swapChain.As(&m_swapChain));
+	m_frameIndex = m_swapChain->GetCurrentBackBufferIndex();
+}
+
+void GameFramework::CreateRtvDsvDescriptorHeap()
+{
+	D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc{};
+	rtvHeapDesc.NumDescriptors = SwapChainBufferCount;
+	rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+	rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+	rtvHeapDesc.NodeMask = 0;
+	DX::ThrowIfFailed(m_device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&m_rtvHeap)));
+	m_rtvDescriptorSize = m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+
+	D3D12_DESCRIPTOR_HEAP_DESC dsvHeapDesc{};
+	dsvHeapDesc.NumDescriptors = 1;
+	dsvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_DSV;
+	dsvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+	dsvHeapDesc.NodeMask = 0;
+	DX::ThrowIfFailed(m_device->CreateDescriptorHeap(&dsvHeapDesc, IID_PPV_ARGS(&m_dsvHeap)));
+}
+
+void GameFramework::CreateRenderTargetView()
+{
+	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHeapHandle{ m_rtvHeap->GetCPUDescriptorHandleForHeapStart() };
+	for (UINT i = 0; i < SwapChainBufferCount; ++i)
+	{
+		// 스왑 체인의 i번째 버퍼를 얻는다.
+		m_swapChain->GetBuffer(i, IID_PPV_ARGS(&m_renderTargets[i]));
+
+		// 그 버퍼에 대한 렌더 타겟 뷰를 생성한다.
+		m_device->CreateRenderTargetView(m_renderTargets[i].Get(), NULL, rtvHeapHandle);
+
+		// 힙의 다음 항목으로 넘어간다.
+		rtvHeapHandle.Offset(1, m_rtvDescriptorSize);
+	}
+}
+
+void GameFramework::CreateDepthStencilView()
+{
+	D3D12_RESOURCE_DESC depthStencilDesc{};
+	depthStencilDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+	depthStencilDesc.Alignment = 0;
+	depthStencilDesc.Width = m_width;
+	depthStencilDesc.Height = m_height;
+	depthStencilDesc.DepthOrArraySize = 1;
+	depthStencilDesc.MipLevels = 1;
+	depthStencilDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	depthStencilDesc.SampleDesc.Count = m_MSAA4xQualityLevel > 1 ? 4 : 1;
+	depthStencilDesc.SampleDesc.Quality = m_MSAA4xQualityLevel > 1 ? m_MSAA4xQualityLevel - 1 : 0;
+	depthStencilDesc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
+	depthStencilDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL;
+
+	D3D12_CLEAR_VALUE optClear{};
+	optClear.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	optClear.DepthStencil.Depth = 1.0f;
+	optClear.DepthStencil.Stencil = 0;
+
+	DX::ThrowIfFailed(m_device->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+		D3D12_HEAP_FLAG_NONE,
+		&depthStencilDesc,
+		D3D12_RESOURCE_STATE_DEPTH_WRITE,
+		&optClear,
+		IID_PPV_ARGS(m_depthStencil.GetAddressOf())));
+
+	// D3D12_DEPTH_STENCIL_VIEW_DESC 구조체는 깊이 스텐실 뷰를 서술하는데, 
+	// 자원에 담긴 원소들의 자료 형식에 관한 멤버를 가지고 있다.
+	D3D12_DEPTH_STENCIL_VIEW_DESC depthStencilViewDesc{};
+	depthStencilViewDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	depthStencilViewDesc.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2D;
+	depthStencilViewDesc.Flags = D3D12_DSV_FLAG_NONE;
+	m_device->CreateDepthStencilView(m_depthStencil.Get(), &depthStencilViewDesc, m_dsvHeap->GetCPUDescriptorHandleForHeapStart());
+}
+
+void GameFramework::CreateRootSignature()
+{
+	CD3DX12_DESCRIPTOR_RANGE descriptorRange[11];
+	descriptorRange[0].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 0, 0, D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND);	// t0
+	descriptorRange[1].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 1, 0, D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND);	// t1
+	descriptorRange[2].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 2, 0, D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND);	// t2
+	descriptorRange[3].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 3, 0, D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND);	// t3
+
+	descriptorRange[4].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 4, 0, D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND);	// t4
+	descriptorRange[5].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 5, 0, D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND);	// t5
+	descriptorRange[6].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 6, 0, D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND);	// t6
+	descriptorRange[7].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 7, 0, D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND);	// t7
+	descriptorRange[8].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 8, 0, D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND);	// t8
+	descriptorRange[9].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 9, 0, D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND);	// t9
+	descriptorRange[10].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 10, 0, D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND);	// t10
+
+	CD3DX12_ROOT_PARAMETER rootParameter[13];
+
+	// cbGameObject : 월드 변환 행렬(16) + struct Material(16) + material type(1)
+	rootParameter[0].InitAsConstants(33, 0, 0, D3D12_SHADER_VISIBILITY_ALL);
+
+	// cbCamera
+	rootParameter[1].InitAsConstantBufferView(1, 0, D3D12_SHADER_VISIBILITY_ALL);
+
+	rootParameter[2].InitAsDescriptorTable(1, &descriptorRange[0], D3D12_SHADER_VISIBILITY_PIXEL);
+	rootParameter[3].InitAsDescriptorTable(1, &descriptorRange[1], D3D12_SHADER_VISIBILITY_PIXEL);
+	rootParameter[4].InitAsDescriptorTable(1, &descriptorRange[2], D3D12_SHADER_VISIBILITY_PIXEL);
+	rootParameter[5].InitAsDescriptorTable(1, &descriptorRange[3], D3D12_SHADER_VISIBILITY_PIXEL);
+
+	rootParameter[6].InitAsDescriptorTable(1, &descriptorRange[4], D3D12_SHADER_VISIBILITY_PIXEL);
+	rootParameter[7].InitAsDescriptorTable(1, &descriptorRange[5], D3D12_SHADER_VISIBILITY_PIXEL);
+	rootParameter[8].InitAsDescriptorTable(1, &descriptorRange[6], D3D12_SHADER_VISIBILITY_PIXEL);
+	rootParameter[9].InitAsDescriptorTable(1, &descriptorRange[7], D3D12_SHADER_VISIBILITY_PIXEL);
+	rootParameter[10].InitAsDescriptorTable(1, &descriptorRange[8], D3D12_SHADER_VISIBILITY_PIXEL);
+	rootParameter[11].InitAsDescriptorTable(1, &descriptorRange[9], D3D12_SHADER_VISIBILITY_PIXEL);
+	rootParameter[12].InitAsDescriptorTable(1, &descriptorRange[10], D3D12_SHADER_VISIBILITY_PIXEL);
+	
+	CD3DX12_STATIC_SAMPLER_DESC samplerDesc[2];
+	samplerDesc[0].Init(								// s0
+		0,								 				// ShaderRegister
+		D3D12_FILTER_MIN_MAG_MIP_LINEAR, 				// filter
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP, 				// addressU
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP, 				// addressV
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP, 				// addressW
+		0.0f,											// mipLODBias
+		1,												// maxAnisotropy
+		D3D12_COMPARISON_FUNC_ALWAYS,					// comparisonFunc
+		D3D12_STATIC_BORDER_COLOR_TRANSPARENT_BLACK,	// borderColor
+		0.0f,											// minLOD
+		D3D12_FLOAT32_MAX,								// maxLOD
+		D3D12_SHADER_VISIBILITY_PIXEL,					// shaderVisibility
+		0												// registerSpace
+	);
+
+	samplerDesc[1].Init(								// s1
+		1,								 				// ShaderRegister
+		D3D12_FILTER_MIN_MAG_MIP_LINEAR, 				// filter
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP, 				// addressU
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP, 				// addressV
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP, 				// addressW
+		0.0f,											// mipLODBias
+		1,												// maxAnisotropy
+		D3D12_COMPARISON_FUNC_ALWAYS,					// comparisonFunc
+		D3D12_STATIC_BORDER_COLOR_TRANSPARENT_BLACK,	// borderColor
+		0.0f,											// minLOD
+		D3D12_FLOAT32_MAX,								// maxLOD
+		D3D12_SHADER_VISIBILITY_PIXEL,					// shaderVisibility
+		0												// registerSpace
+	);
+
+	CD3DX12_ROOT_SIGNATURE_DESC rootSignatureDesc;
+	rootSignatureDesc.Init(_countof(rootParameter), rootParameter, _countof(samplerDesc), samplerDesc, D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT);
+
+	ComPtr<ID3DBlob> signature, error;
+	DX::ThrowIfFailed(D3D12SerializeRootSignature(&rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1, &signature, &error));
+	DX::ThrowIfFailed(m_device->CreateRootSignature(0, signature->GetBufferPointer(), signature->GetBufferSize(), IID_PPV_ARGS(&m_rootSignature)));
+}
+
+
+void GameFramework::BuildObjects()
+{
+	m_commandList->Reset(m_commandAllocator.Get(), nullptr);
+
+	m_scene = make_unique<Scene>();
+	m_scene->BuildObjects(m_device, m_commandList, m_rootSignature, m_aspectRatio);
+
+	// 명령 제출
+	m_commandList->Close();
+	ID3D12CommandList* ppCommandList[] = { m_commandList.Get() };
+	m_commandQueue->ExecuteCommandLists(_countof(ppCommandList), ppCommandList);
+
+	// 명령들이 완료될 때까지 대기
+	WaitForGpuComplete();
+
+	// 디폴트 버퍼로의 복사가 완료됐으므로 업로드 버퍼를 해제한다.
+	for (const auto& shader : m_scene->GetShaders())
+		shader.second->ReleaseUploadBuffer();
+
+	m_timer.Tick();
+}
+
+void GameFramework::FrameAdvance()
+{
+	m_timer.Tick();
+
+	if (m_isActive)
+	{
+		OnProcessingMouseMessage();
+		OnProcessingKeyboardMessage();
+	}
+	Update(m_timer.GetDeltaTime());
+	Render();
+}
+
+void GameFramework::Update(FLOAT timeElapsed)
+{
+	wstring title{ TEXT("project15 (") + to_wstring((int)(m_timer.GetFPS())) + TEXT("FPS)") };
+	SetWindowText(m_hWnd, title.c_str());
+
+	if (m_scene) m_scene->Update(timeElapsed);
+}
+
+void GameFramework::WaitForGpuComplete()
+{
+	//GPU가 펜스의 값을 설정하는 명령을 명령 큐에 추가한다. 
+	const UINT64 fence = m_fenceValue;
+	DX::ThrowIfFailed(m_commandQueue->Signal(m_fence.Get(), fence));
+	//CPU 펜스의 값을 증가시킨다. 
+	++m_fenceValue;
+
+	//펜스의 현재 값이 설정한 값보다 작으면 펜스의 현재 값이 설정한 값이 될 때까지 기다린다. 
+	if (m_fence->GetCompletedValue() < fence)
+	{
+		DX::ThrowIfFailed(m_fence->SetEventOnCompletion(fence, m_fenceEvent));
+		::WaitForSingleObject(m_fenceEvent, INFINITE);
+	}
+	m_frameIndex = m_swapChain->GetCurrentBackBufferIndex();
+}
+
+void GameFramework::Render()
+{
+	// 명령 할당자와 명령 리스트를 리셋한다. 
+	DX::ThrowIfFailed(m_commandAllocator->Reset());
+	DX::ThrowIfFailed(m_commandList->Reset(m_commandAllocator.Get(), nullptr));
+
+	// 자원 용도와 관련된 상태 전이를 Direct3D에 통지한다.
+	m_commandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(m_renderTargets[m_frameIndex].Get(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET));
+
+	//뷰포트와 씨저 사각형을 설정한다. 
+	m_commandList->SetGraphicsRootSignature(m_rootSignature.Get());
+	m_commandList->RSSetViewports(1, &m_viewport);
+	m_commandList->RSSetScissorRects(1, &m_scissorRect);
+
+	//현재의 렌더 타겟에 해당하는 서술자와 깊이 스텐실 서술자의 CPU 주소(핸들)를 계산한다. 
+	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle{ m_rtvHeap->GetCPUDescriptorHandleForHeapStart(), static_cast<INT>(m_frameIndex), m_rtvDescriptorSize };
+	CD3DX12_CPU_DESCRIPTOR_HANDLE dsvHandle{ m_dsvHeap->GetCPUDescriptorHandleForHeapStart() };
+	m_commandList->OMSetRenderTargets(1, &rtvHandle, TRUE, &dsvHandle);
+
+	// 원하는 색상으로 렌더 타겟을 지우고, 원하는 값으로 깊이 스텐실을 지운다.
+	const FLOAT clearColor[]{ 0.0f, 0.125f, 0.3f, 1.0f };
+	m_commandList->ClearRenderTargetView(rtvHandle, clearColor, 0, NULL);
+	m_commandList->ClearDepthStencilView(dsvHandle, D3D12_CLEAR_FLAG_DEPTH | D3D12_CLEAR_FLAG_STENCIL, 1.0f, 0, 0, NULL);
+
+	// Scene을 Render한다.
+	if (m_scene) m_scene->Render(m_commandList);
+
+	// 자원 용도와 관련된 상태 전이를 Direct3D에 통지한다.
+	m_commandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(m_renderTargets[m_frameIndex].Get(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT));
+
+	// 명령들의 기록을 마친다.
+	DX::ThrowIfFailed(m_commandList->Close());
+
+
+	// 명령 실행을 위해 커맨드 리스트를 커맨드 큐에 추가한다.
+	ID3D12CommandList* ppCommandList[] = { m_commandList.Get() };
+	m_commandQueue->ExecuteCommandLists(_countof(ppCommandList), ppCommandList);
+
+	DX::ThrowIfFailed(m_swapChain->Present(1, 0));
+
+	WaitForGpuComplete();
+}
---
+++ d3d12-framework-project/project15-1/scene.cpp
@@ -0,0 +1,231 @@
+#include "scene.h"
+
+
+Scene::~Scene()
+{
+}
+
+void Scene::OnProcessingMouseMessage(HWND hWnd, UINT width, UINT height, FLOAT deltaTime) const
+{
+	SetCursor(NULL);
+	RECT rect; GetWindowRect(hWnd, &rect);
+	POINT prevPosition{ rect.left + width / 2, rect.top + height / 2 };
+
+	POINT nextPosition; GetCursorPos(&nextPosition);
+
+	int dx = nextPosition.x - prevPosition.x;
+	int dy = nextPosition.y - prevPosition.y;
+	m_player->Rotate(dy * 5.0f * deltaTime, dx * 5.0f * deltaTime, 0.0f);
+	SetCursorPos(prevPosition.x, prevPosition.y);
+}
+
+void Scene::OnProcessingKeyboardMessage(FLOAT timeElapsed) const
+{
+	if (GetAsyncKeyState('W') & 0x8000)
+	{
+		m_player->AddVelocity(Vector3::Mul(m_player->GetFront(), timeElapsed * 10.0f));
+	}
+	if (GetAsyncKeyState('A') & 0x8000)
+	{
+		m_player->AddVelocity(Vector3::Mul(m_player->GetRight(), timeElapsed * -10.0f));
+	}
+	if (GetAsyncKeyState('S') & 0x8000)
+	{
+		m_player->AddVelocity(Vector3::Mul(m_player->GetFront(), timeElapsed * -10.0f));
+	}
+	if (GetAsyncKeyState('D') & 0x8000)
+	{
+		m_player->AddVelocity(Vector3::Mul(m_player->GetRight(), timeElapsed * 10.0f));
+	}
+	if (GetAsyncKeyState(VK_SPACE) & 0x8000)
+	{
+		m_player->AddVelocity(Vector3::Mul(m_player->GetUp(), timeElapsed * 10.0f));
+	}
+	if (GetAsyncKeyState(VK_SHIFT) & 0x8000)
+	{
+		m_player->AddVelocity(Vector3::Mul(m_player->GetUp(), timeElapsed * -10.0f));
+	}
+}
+
+void Scene::BuildObjects(const ComPtr<ID3D12Device>& device, const ComPtr<ID3D12GraphicsCommandList>& commandlist, const ComPtr<ID3D12RootSignature>& rootsignature, FLOAT aspectRatio)
+{
+	unique_ptr<TextureHierarchyShader> hierarchyShader{ make_unique<TextureHierarchyShader>(device, rootsignature) };
+
+	// 플레이어 생성
+	m_player = make_shared<Player>();
+	m_player->LoadGeometry(device, commandlist, TEXT("Model/SuperCobra.bin"));
+	m_player->SetRotorFrame();
+	m_player->SetPosition(XMFLOAT3(100.0f, 100.0f, 65.0f));
+	m_player->SetScale(0.3f, 0.3f, 0.3f);
+	hierarchyShader->SetPlayer(m_player);
+
+	// 카메라 생성
+	m_camera = make_shared<ThirdPersonCamera>();
+	m_camera->CreateShaderVariable(device, commandlist);
+	m_camera->SetEye(XMFLOAT3{ 0.0f, 0.0f, 0.0f });
+	m_camera->SetAt(XMFLOAT3{ 0.0f, 0.0f, 1.0f });
+	m_camera->SetUp(XMFLOAT3{ 0.0f, 1.0f, 0.0f });
+	m_camera->SetPlayer(m_player);
+	m_player->SetCamera(m_camera);
+
+	XMFLOAT4X4 projMatrix;
+	XMStoreFloat4x4(&projMatrix, XMMatrixPerspectiveFovLH(0.25f * XM_PI, aspectRatio, 0.1f, 1000.0f));
+	m_camera->SetProjMatrix(projMatrix);
+
+	// 적 생성
+	auto enemyPoint1 = make_shared<EnemyManager>(device, commandlist);
+	enemyPoint1->SetPosition(XMFLOAT3{ 0.0f, 0.0f, 0.0f });
+	hierarchyShader->GetGameObjects().push_back(enemyPoint1);
+
+	// 지형 생성
+	unique_ptr<TerrainShader> terrainShader{ make_unique<TerrainShader>(device, rootsignature) };
+	shared_ptr<HeightMapTerrain> terrain{
+		make_shared<HeightMapTerrain>(device, commandlist, TEXT("Resource/HeightMap/HeightMap.raw"), 257, 257, 257, 257, XMFLOAT3{ 1.0f, 0.1f, 1.0f })
+	};
+	shared_ptr<Texture> terrainTexture{
+		make_shared<Texture>()
+	};
+	terrainTexture->LoadTextureFile(device, commandlist, TEXT("Resource/Texture/Base_Texture.dds"), 2); // BaseTexture
+	terrainTexture->LoadTextureFile(device, commandlist, TEXT("Resource/Texture/Detail_Texture.dds"), 3); // DetailTexture
+	terrainTexture->CreateSrvDescriptorHeap(device);
+	terrainTexture->CreateShaderResourceView(device, D3D12_SRV_DIMENSION_TEXTURE2D);
+	terrain->SetPosition(XMFLOAT3{ 0.0f, 0.0f, 0.0f });
+	terrain->SetTexture(terrainTexture);
+	terrainShader->SetTerrain(terrain);
+	m_player->SetTerrain(terrain);
+
+	// 스카이박스 생성
+	unique_ptr<SkyboxShader> skyboxShader = make_unique<SkyboxShader>(device, rootsignature);
+	shared_ptr<Skybox> skybox{ make_shared<Skybox>(device, commandlist, 20.0f, 20.0f, 20.0f) };
+	shared_ptr<Texture> skyboxTexture{
+		make_shared<Texture>()
+	};
+	skyboxTexture->LoadTextureFile(device, commandlist, TEXT("Resource/Texture/SkyBox.dds"), 4);
+	skyboxTexture->CreateSrvDescriptorHeap(device);
+	skyboxTexture->CreateShaderResourceView(device, D3D12_SRV_DIMENSION_TEXTURECUBE);
+	skybox->SetTexture(skyboxTexture);
+	skyboxShader->GetGameObjects().push_back(skybox);
+
+	// 물 지형 생성
+	unique_ptr<BlendingShader> blendingShader = make_unique<BlendingShader>(device, rootsignature);
+	shared_ptr<GameObject> river = make_shared<GameObject>();
+	shared_ptr<TextureRectMesh> riverMesh = make_shared<TextureRectMesh>(device, commandlist, XMFLOAT3{0.f, 0.f, 0.f}, 20.f, 0.f, 20.f);
+	river->SetMesh(riverMesh);
+	shared_ptr<Texture> riverTexture{
+		make_shared<Texture>()
+	};
+	riverTexture->LoadTextureFile(device, commandlist, TEXT("Resource/Texture/Water.dds"), 5);
+	riverTexture->CreateSrvDescriptorHeap(device);
+	riverTexture->CreateShaderResourceView(device, D3D12_SRV_DIMENSION_TEXTURE2D);
+	auto riverMaterial = make_shared<Material>();
+	riverMaterial->m_albedoColor.w = 0.51f;
+	river->SetScale(13.f, 0.f, 13.f);
+	river->SetMaterial(riverMaterial);
+	river->SetPosition(XMFLOAT3{ 125.0f, 70.0f, 125.0f });
+	river->SetTexture(riverTexture);
+	blendingShader->GetGameObjects().push_back(river);
+
+	// 빌보드 생성
+	vector<shared_ptr<GameObject>> vflower1;
+	vector<shared_ptr<GameObject>> vflower2;
+	vector<shared_ptr<GameObject>> vgrass1;
+	vector<shared_ptr<GameObject>> vgrass2;
+	auto flower1Mesh = make_shared<BillBoardMesh>(device, commandlist, XMFLOAT3{ 0.f, 0.f, 0.f }, XMFLOAT2{ 1.f, 1.f });
+	auto flower2Mesh = make_shared<BillBoardMesh>(device, commandlist, XMFLOAT3{ 0.f, 0.f, 0.f }, XMFLOAT2{ 1.f, 1.f });
+	auto grass1Mesh = make_shared<BillBoardMesh>(device, commandlist, XMFLOAT3{ 0.f, 0.f, 0.f }, XMFLOAT2{ 1.f, 1.f });
+	auto grass2Mesh = make_shared<BillBoardMesh>(device, commandlist, XMFLOAT3{ 0.f, 0.f, 0.f }, XMFLOAT2{ 1.f, 1.f });
+	auto flower1Texture { make_shared<Texture>() };
+	flower1Texture->LoadTextureFile(device, commandlist, TEXT("Resource/Texture/Flower01.dds"), 2);
+	flower1Texture->CreateSrvDescriptorHeap(device);
+	flower1Texture->CreateShaderResourceView(device, D3D12_SRV_DIMENSION_TEXTURE2D);
+	auto flower2Texture{ make_shared<Texture>() };
+	flower2Texture->LoadTextureFile(device, commandlist, TEXT("Resource/Texture/Flower02.dds"), 2);
+	flower2Texture->CreateSrvDescriptorHeap(device);
+	flower2Texture->CreateShaderResourceView(device, D3D12_SRV_DIMENSION_TEXTURE2D);
+	auto grass1Texture{ make_shared<Texture>() };
+	grass1Texture->LoadTextureFile(device, commandlist, TEXT("Resource/Texture/Grass01.dds"), 2);
+	grass1Texture->CreateSrvDescriptorHeap(device);
+	grass1Texture->CreateShaderResourceView(device, D3D12_SRV_DIMENSION_TEXTURE2D);
+	auto grass2Texture{ make_shared<Texture>() };
+	grass2Texture->LoadTextureFile(device, commandlist, TEXT("Resource/Texture/Grass02.dds"), 2);
+	grass2Texture->CreateSrvDescriptorHeap(device);
+	grass2Texture->CreateShaderResourceView(device, D3D12_SRV_DIMENSION_TEXTURE2D);
+	XMFLOAT3 terrainPosition = terrain->GetPosition();
+	XMFLOAT3 terrainScale = terrain->GetScale();
+	FLOAT terrainHeight = terrain->GetHeight(terrainPosition.x, terrainPosition.z);
+	for (int z = 2; z <= 256; ++z) {
+		FLOAT nz = ((terrainPosition.z + (terrain->GetLength() / 256 * z)));
+		for (int x = 2; x <= 256; ++x) {
+			FLOAT nx = ((terrainPosition.x + (terrain->GetWidth() / 256 * x)));
+			FLOAT ny = terrain->GetHeight(nx, nz) / terrainScale.y + 0.4f;
+			if (ny > 70.f) {
+				int value = GetRandomNumber(1, 4);
+				if (value == 1) {
+					auto flower1 = make_shared<GameObject>();
+					flower1->SetPosition(XMFLOAT3{ nx, ny, nz });
+					vflower1.push_back(flower1);
+				}
+				if (value == 2) {
+					auto flower2 = make_shared<GameObject>();
+					flower2->SetPosition(XMFLOAT3{ nx, ny, nz });
+					vflower2.push_back(flower2);
+				}
+				if (value == 3) {
+					auto grass1 = make_shared<GameObject>();
+					grass1->SetPosition(XMFLOAT3{ nx, ny, nz });
+					vgrass1.push_back(grass1);
+				}
+				if (value == 4) {
+					auto grass2 = make_shared<GameObject>();
+					grass2->SetPosition(XMFLOAT3{ nx, ny, nz });
+					vgrass2.push_back(grass2);
+				}
+			}
+		}
+	}
+	unique_ptr<BillBoardShader> flower1Shader = make_unique<BillBoardShader>(device, rootsignature, vflower1.size());
+	unique_ptr<BillBoardShader> flower2Shader = make_unique<BillBoardShader>(device, rootsignature, vflower2.size());
+	unique_ptr<BillBoardShader> grass1Shader = make_unique<BillBoardShader>(device, rootsignature, vgrass1.size());
+	unique_ptr<BillBoardShader> grass2Shader = make_unique<BillBoardShader>(device, rootsignature, vgrass2.size());
+	for (const auto& obj : vflower1) flower1Shader->GetGameObjects().push_back(obj);
+	for (const auto& obj : vflower2) flower2Shader->GetGameObjects().push_back(obj);
+	for (const auto& obj : vgrass1) grass1Shader->GetGameObjects().push_back(obj);
+	for (const auto& obj : vgrass2) grass2Shader->GetGameObjects().push_back(obj);
+	flower1Shader->SetMesh(flower1Mesh);
+	flower2Shader->SetMesh(flower2Mesh);
+	grass1Shader->SetMesh(grass1Mesh);
+	grass2Shader->SetMesh(grass2Mesh);
+	flower1Shader->SetTexture(flower1Texture);
+	flower2Shader->SetTexture(flower2Texture);
+	grass1Shader->SetTexture(grass1Texture);
+	grass2Shader->SetTexture(grass2Texture);
+
+	// 셰이더 설정
+	m_shader.insert(make_pair("HIERARCHY", move(hierarchyShader)));
+	m_shader.insert(make_pair("TERRAIN", move(terrainShader)));
+	m_shader.insert(make_pair("SKYBOX", move(skyboxShader)));
+	m_blending.insert(make_pair("FLOWER1", move(flower1Shader)));
+	m_blending.insert(make_pair("FLOWER2", move(flower2Shader)));
+	m_blending.insert(make_pair("GRASS1", move(grass1Shader)));
+	m_blending.insert(make_pair("GRASS2", move(grass2Shader)));
+	m_blending.insert(make_pair("BLENDING", move(blendingShader)));
+}
+
+void Scene::Update(FLOAT timeElapsed)
+{
+	m_camera->Update(timeElapsed);
+	if (m_shader["SKYBOX"]) for (auto& skybox : m_shader["SKYBOX"]->GetGameObjects()) skybox->SetPosition(m_camera->GetEye());
+	for (const auto& shader : m_shader) shader.second->Update(timeElapsed);
+	for (const auto& shader : m_blending) shader.second->Update(timeElapsed);
+}
+
+void Scene::Render(const ComPtr<ID3D12GraphicsCommandList>& commandList) const
+{
+	if (m_camera) m_camera->UpdateShaderVariable(commandList);
+	for (const auto& shader : m_shader) shader.second->Render(commandList);
+	m_blending.at("BLENDING")->Render(commandList);
+	m_blending.at("FLOWER1")->Render(commandList);
+	m_blending.at("FLOWER2")->Render(commandList);
+	m_blending.at("GRASS1")->Render(commandList);
+	m_blending.at("GRASS2")->Render(commandList);
+}
